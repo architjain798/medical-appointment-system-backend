71005f78e881937099ca5a24b410906d571a7cd2
Archit Jain <architjain798@gmail.com>
2024-12-12 16:32:49 +0530

    Refactor database connection and server setup for improved configuration

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3010
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Connect to MongoDB
+connectDB();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
