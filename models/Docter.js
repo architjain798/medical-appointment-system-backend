151ff3776a4651e0cf3a715ff4fb5a1d066c8c5f
Archit Jain <architjain798@gmail.com>
2024-12-14 18:58:16 +0530

    Refactor doctor schedule schema to use an array of objects for date and time management

--- models/Docter.js
+++ models/Docter.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const doctorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   specialty: { type: String, required: true },
-  schedule: { type: Map, of: [String] }, // e.g., { Monday: ['10AM', '11AM'] }
+  schedule: [
+    {
+      date: { type: String, required: true }, // e.g., "2024-12-20"
+      start: { type: String, required: true }, // e.g., "09:00"
+      end: { type: String, required: true } // e.g., "17:00"
+    }
+  ]
 });
 
 module.exports = mongoose.model('Doctor', doctorSchema);
